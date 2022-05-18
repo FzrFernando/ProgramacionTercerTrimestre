efe7839b25880fd28f7a1a0aa795c71e86fe9c8a
FzrFernando <74764616+FzrFernando@users.noreply.github.com>
2022-05-18 18:29:58 +0200

    Casetas terminada

--- Casetas/src/com/jacaranda/casetaferia/Calle.java
+++ Casetas/src/com/jacaranda/casetaferia/Calle.java
@@ -6,91 +6,106 @@ import java.util.Objects;
 
 public class Calle {
 	private String calle;
-	private List<Caseta>casetas;
+	private List<Caseta> casetas;
+
 	public Calle(String calle) {
 		super();
 		this.calle = calle;
-		this.casetas=new ArrayList<>();
+		this.casetas = new ArrayList<>();
 	}
+
 	public void annadirCaseta(Caseta c) {
 		this.casetas.add(c);
 	}
-	public String getNumeroCalle() {
+
+	public String getCalle() {
 		return calle;
 	}
+
 	public void setNumeroCalle(String calle) {
 		this.calle = calle;
 	}
+
 	public Caseta getCasetas(String calle) {
-		Caseta aux=new Caseta(calle);
-		int posicion=this.casetas.indexOf(aux);
-		if (posicion==-1) {
+		Caseta aux = new Caseta(calle);
+		int posicion = this.casetas.indexOf(aux);
+		if (posicion == -1) {
 			return null;
-		}else {
+		} else {
 			return this.casetas.get(posicion);
 		}
-		
+
 	}
+
 	public void setCasetas(List<Caseta> casetas) {
 		this.casetas = casetas;
 	}
+
 	@Override
 	public String toString() {
-		return "Calle [numeroCalle=" + calle + ", casetas=" + casetas + "]";
+		return "Calle numeroCalle=" + calle + ", casetas=" + casetas + "]";
 	}
+
 	public String mostrarCasetasCalle(String calle) {
-		StringBuilder resultado=new StringBuilder();
-		for (Caseta siguiente:this.casetas) {
+		StringBuilder resultado = new StringBuilder();
+		for (Caseta siguiente : this.casetas) {
 			if (siguiente.getCalle().equals(calle)) {
 				resultado.append(siguiente);
 			}
 		}
 		return resultado.toString();
 	}
+
 	public String mostrarFamiliar() {
-		StringBuilder aux=new StringBuilder();
-		for (Caseta siguiente:this.casetas) {
+		StringBuilder aux = new StringBuilder();
+		for (Caseta siguiente : this.casetas) {
 			if (siguiente.getClase().toUpperCase().equals("FAMILIAR")) {
 				aux.append(siguiente);
 			}
 		}
 		return aux.toString();
 	}
+
 	public String mostrarDistrito() {
-		StringBuilder aux=new StringBuilder();
-		for (Caseta siguiente:this.casetas) {
+		StringBuilder aux = new StringBuilder();
+		for (Caseta siguiente : this.casetas) {
 			if (siguiente.getClase().toUpperCase().equals("DISTRITO")) {
 				aux.append(siguiente);
 			}
 		}
 		return aux.toString();
 	}
+
 	public String mostrarDistintoFamiliarYDistrito() {
-		StringBuilder aux=new StringBuilder();
-		for (Caseta siguiente:this.casetas) {
-			if (!"DISTRITO".equalsIgnoreCase(siguiente.getClase().toUpperCase())&&!"FAMILIAR".equalsIgnoreCase(siguiente.getClase().toUpperCase())) {
+		StringBuilder aux = new StringBuilder();
+		for (Caseta siguiente : this.casetas) {
+			if (!"DISTRITO".equalsIgnoreCase(siguiente.getClase().toUpperCase())
+					&& !"FAMILIAR".equalsIgnoreCase(siguiente.getClase().toUpperCase())) {
 				aux.append(siguiente);
 			}
 		}
 		return aux.toString();
 	}
-	public boolean eliminarCaseta(String idCaseta) throws CalleException {
-		boolean eliminado=false;
-		Caseta aux=new Caseta(idCaseta);
-		int posicion=this.casetas.indexOf(aux);
-		if (posicion!=-1) {
+
+	public boolean eliminarCaseta(String nombre) throws CalleException {
+		boolean eliminado = false;
+		Caseta aux = new Caseta(nombre);
+		int posicion = this.casetas.indexOf(aux);
+		if (posicion != -1) {
 			this.casetas.remove(posicion);
-			eliminado=true;
-		}else {
+			eliminado = true;
+		} else {
 			throw new CalleException("Error, no se encuentra la caseta");
 		}
 		return eliminado;
-		
+
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(calle);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -102,4 +117,5 @@ public class Calle {
 		Calle other = (Calle) obj;
 		return Objects.equals(calle, other.calle);
 	}
+
 }
--- Casetas/src/com/jacaranda/casetaferia/Caseta.java
+++ Casetas/src/com/jacaranda/casetaferia/Caseta.java
@@ -10,6 +10,7 @@ public class Caseta {
 	private String clase;
 	private int id;
 	private int id_calle;
+
 	public Caseta(String titulo, String calle, int numero, int modulo, String clase, int id, int id_calle) {
 		super();
 		this.titulo = titulo;
@@ -21,61 +22,72 @@ public class Caseta {
 		this.id_calle = id_calle;
 	}
 
-
-
-	public Caseta(String calle) {
+	public Caseta(String titulo) {
 		super();
-		this.calle = calle;
+		this.titulo = titulo;
 	}
-
-
 
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getCalle() {
 		return calle;
 	}
+
 	public void setCalle(String calle) {
 		this.calle = calle;
 	}
+
 	public int getNumero() {
 		return numero;
 	}
+
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
+
 	public int getModulos() {
 		return modulos;
 	}
+
 	public void setModulos(int modulos) {
 		this.modulos = modulos;
 	}
+
 	public String getClase() {
 		return clase;
 	}
+
 	public void setClase(String clase) {
 		this.clase = clase;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public int getId_calle() {
 		return id_calle;
 	}
+
 	public void setId_calle(int id_calle) {
 		this.id_calle = id_calle;
 	}
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		return Objects.hash(titulo);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -85,11 +97,13 @@ public class Caseta {
 		if (getClass() != obj.getClass())
 			return false;
 		Caseta other = (Caseta) obj;
-		return id == other.id;
+		return Objects.equals(titulo, other.titulo);
 	}
+
 	@Override
 	public String toString() {
-		return "Caseta [titulo=" + titulo + ", calle=" + calle + ", numero=" + numero + ", modulos=" + modulos + ", clase="
-				+ clase + ", id=" + id + ", id_calle=" + id_calle + "]";
+		return "Caseta [titulo=" + titulo + ", calle=" + calle + ", numero=" + numero + ", modulos=" + modulos
+				+ ", clase=" + clase + ", id=" + id + ", id_calle=" + id_calle + "]" + "\n";
 	}
+
 }
