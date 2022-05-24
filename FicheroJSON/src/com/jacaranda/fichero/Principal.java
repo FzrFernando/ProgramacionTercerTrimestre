b2303984a9a768cb822d0c07b2436401273828e6
FzrFernando <fernandozamorarico@gmail.com>
2022-05-24 08:47:45 +0200

    Ejemplos JSON

--- FicheroJSON/src/com/jacaranda/fichero/Principal.java
+++ FicheroJSON/src/com/jacaranda/fichero/Principal.java
@@ -3,26 +3,53 @@ package com.jacaranda.fichero;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Properties;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		String fichero = imprimirFicheroPorLineas("ficheros/prueba1.json");
+		String fichero = imprimirFicheroPorLineas("ficheros/prueba2.json");
 		System.out.println("************************");
 		System.out.println(fichero);
-		
+
 		Gson gson = new Gson();
+
+//		Properties properties = gson.fromJson(fichero, Properties.class);
+//		
+//		System.out.println(properties.get("nombre"));
+//		System.out.println(properties.get("apellidos"));
+//		System.out.println(properties.get("edad"));
+
+//		Persona p1 = gson.fromJson(fichero, Persona.class);
+//		System.out.println(p1);
+
+		ArrayList<Persona> personas = gson.fromJson(fichero, new TypeToken<ArrayList<Persona>>() {
+		}.getType());
+
+		for (Persona p : personas) {
+			System.out.println(p.toString());
+		}
+
+		Persona p1 = new Persona("Pepe", "Pérez", 20);
+		personas.add(p1);
+		personas.remove(0);
+
+//		String salida = gson.toJson(personas);
+//		System.out.println("Informaciñon del string");
+//		System.out.println(salida);
 		
-		Properties properties = gson.fromJson(fichero, Properties.class);
+		Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
+		String salida = prettyGson.toJson(personas);
 		
-		System.out.println(properties.get("nombre"));
-		System.out.println(properties.get("apellidos"));
-		System.out.println(properties.get("edad"));
-		
+		escribirEnFicheroJson("ficheros/prueba3.json", salida);
 	}
 
 	private static String imprimirFicheroPorLineas(String nombreFichero) {
@@ -47,4 +74,18 @@ public class Principal {
 		return resultado.toString();
 	}
 
+	private static void escribirEnFicheroJson(String nombre, String contenido) {
+		String cadena;
+		try {
+			FileWriter flujoEscritura = new FileWriter(nombre);
+			PrintWriter filtroEscritura = new PrintWriter(flujoEscritura);
+
+			filtroEscritura.println(contenido);
+
+			filtroEscritura.close();
+			flujoEscritura.close();
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
