2f07694b87f772b0165ad041cd9091a242cd764c
FzrFernando <74764616+FzrFernando@users.noreply.github.com>
2022-05-27 16:57:09 +0200

    Corregido Diccionario Mapa

--- DiccionarioMapa/src/com/jacaranda/diccionario/Diccionario.java
+++ DiccionarioMapa/src/com/jacaranda/diccionario/Diccionario.java
@@ -2,69 +2,78 @@ package com.jacaranda.diccionario;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Diccionario {
 	
-	private HashMap<String,HashSet<String>> palabras;
+	private HashMap <String, HashSet<String>> palabras;
+
 	
 	public Diccionario() {
 		this.palabras = new HashMap<>();
 	}
 	
-	public boolean anadirPalabra(String palabra, String significado) {
-		boolean resultado= true;
-		//Averiguar si la palabra ya está en el diccionario
-		HashSet<String> valor = this.palabras.get(palabra);
-		if(valor == null) {
-			//Creo el HashSet, le añado el significado y añado al mapa
-			HashSet<String> significados = new HashSet<>();
-			significados.add(significado);
-			if (this.palabras.put(palabra,significados) == null) {
+	public boolean addPalabra(String palabra, String significado) throws DiccionarioException {
+		boolean resultado = true;
+		
+		HashSet<String> significados = this.palabras.get(noEsNulo(palabra).toUpperCase());
+		if(significados==null) {  
+			significados = new HashSet<>();
+			significados.add(noEsNulo(significado).toUpperCase());
+			if(this.palabras.put(palabra.toUpperCase(), significados) == null) {
 				resultado = false;
 			}
+		}else {
+			resultado = significados.add(noEsNulo(significado).toUpperCase());
 		}
-		else { //En el caso que la palabra ya esté en el diccionario
-			resultado = valor.add(significado);
-		}
-		return resultado;
-	}
-
-	public boolean borrarPalabra(String palabra) {
-		boolean resultado;
-		if (this.palabras.remove(palabra) == null) {
-			resultado = false;
-		}
-		else {
-			resultado = true;
-		}
+		
 		return resultado;
 	}
 	
-	public boolean borrarSignificadoPalabra(String palabra, String significado) {
-		boolean resultado;
-		if (this.palabras.get(palabra).remove(significado) == true) {
-			resultado = true;
-		}
-		else {
-			resultado = false;
-		}
-		return resultado;
+	
+	public boolean borrarPalabra(String palabra) throws DiccionarioException {
+		return this.palabras.remove(noEsNulo(palabra).toUpperCase()) != null;
 	}
 	
-	public String buscarPalabra(String principio) {
-		StringBuilder resultado = new StringBuilder();
-		for(String clave : palabras.keySet()) {
-			if (clave.toUpperCase().startsWith(principio)) {
-				resultado.append(clave + "\n");
+	
+	public boolean borrarSignificadoPalabra(String palabra, String significado) throws DiccionarioException {
+		return this.palabras.get(noEsNulo(palabra).toUpperCase()).remove(noEsNulo(significado).toUpperCase());
+	}
+	
+	
+	private String noEsNulo(String palabra) throws DiccionarioException {
+		if(palabra==null) {
+			throw new DiccionarioException("No puede ser un valor nulo.");
+		}
+		return palabra;
+	}
+	
+	
+	public String palabrasQueEmpiecen(String cadena) throws DiccionarioException {
+		if(cadena==null) {
+			throw new DiccionarioException("La cadena no puede ser nula.");
+		}
+		StringBuilder mensaje = new StringBuilder();
+		Iterator iterator = palabras.entrySet().iterator();
+		while(iterator.hasNext()) {
+			Map.Entry entry = (Map.Entry) iterator.next();
+			if(entry.getKey().toString().startsWith(cadena.toUpperCase())) {
+				mensaje.append(entry.getKey() + "\n" + entry.getValue());
 			}
 		}
-		return resultado.toString();
+		if(mensaje.length()==0) {
+			mensaje.append("No existen palabras que empiecen por " + cadena);
+		}
+		return mensaje.toString();
 	}
+	
 	
 	@Override
 	public String toString() {
 		return "Diccionario [palabras=" + palabras + "]";
 	}
+	
 	
 	
 }
--- DiccionarioMapa/src/com/jacaranda/diccionario/Main.java
+++ DiccionarioMapa/src/com/jacaranda/diccionario/Main.java
@@ -4,10 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		Diccionario d1 = new Diccionario();
-		d1.anadirPalabra("Hola", "significado 1");
-		d1.anadirPalabra("Adios", "significado 4");
-		System.out.println(d1);
-		d1.anadirPalabra("Hola", "significado 4");
-		System.out.println(d1);
+		try {
+			d1.addPalabra("Hola", "significado 1");
+			d1.addPalabra("Adios", "significado 4");
+			System.out.println(d1);
+			d1.addPalabra("Hola", "significado 4");
+			System.out.println(d1);
+		} catch (DiccionarioException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
